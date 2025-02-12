56967563f005bcbc82b8cf20706c26620e293a77
Diego9845 <al03085953@tecmilenio.mx>
2025-02-11 19:55:44 -0600

    Add files via upload

---
+++ 2025_02_12_013511_create_gender_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('gender', function (Blueprint $table) {
+            $table->id();
+            $table->string('superheroes_id') ->contrained('superheroes'); 
+            $table->string('male', 20);
+            $table->string('female', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('gender');
+    }
+};
