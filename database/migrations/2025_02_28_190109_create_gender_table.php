9f59a5ef7cc9a918ffbd3efa62bcd7220aee136f
Diego <leeje_08@hotmail.com>
2025-03-11 12:47:22 -0600

    Actividad8

--- database/migrations/2025_02_28_190109_create_gender_table.php
+++ database/migrations/2025_02_28_190109_create_gender_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('gender', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('male', 20);
+            $table->string('female', 20);
             $table->timestamps();
         });
     }
