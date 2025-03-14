f212024eb51b6d2600a9e4b8bddc85cf94439f91
Diego <leeje_08@hotmail.com>
2025-03-13 19:45:52 -0600

    jidef

--- app/Http/Controllers/SuperHeroController.php
+++ app/Http/Controllers/SuperHeroController.php
@@ -48,8 +48,8 @@ class SuperHeroController extends Controller
      */
     public function show(string $id)
     {
-        $superhero = SuperHero::find(101);
-        dd($superhero);
+        $superhero = SuperHero::find($id);
+        return view('superheroes.show', compact('superhero'));
     }
 
     /**
