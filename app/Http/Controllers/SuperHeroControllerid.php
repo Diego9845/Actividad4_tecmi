3e69cf79930a2f401e302a52081719d45a65fada
Diego <leeje_08@hotmail.com>
2025-04-10 15:39:27 -0600

    y

---
+++ app/Http/Controllers/SuperHeroControllerid.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Universe;
+use Illuminate\Http\Request;
+
+class SuperHeroControllerid extends Controller
+{
+    public function index(){
+        $universes = Universe::all();
+        return response()->json($universes);
+    }
+
+    public function show($name){
+        $universe = Universe::wehe('name', $name) -> with('superheroes') -> get();
+        return response()-> json($universe);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,16 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GenderControllerid;
+use App\Http\Controllers\SuperHeroControllerid;
+use illuminate\Support\Facades\Route;
+use App\Http\Controllers\UniverseControllerid;
 
-Route::get('/', function () {
+Route::get('/', function() {
     return view('welcome');
-});
+}
+);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
+Route::get('/universe', [UniverseControllerid::class, 'index']);
+Route::get('/universe/{name}', [UniverseControllerid::class, 'index']);
+Route::get('/gender', [GenderControllerid::class, 'index']);
+Route::get('/superhero', [SuperHeroControllerid::class, 'index']);
